81f80dda4d7c7ed5791acb86c2d7307efa4bc2fc
Gottem <60016015+dgottem@users.noreply.github.com>
2024-02-02 20:58:21 +0100

    Delete topheader.js

--- topheader.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuIcon = document.getElementById('menu-icon');
-    const navbar = document.querySelector('.navbar');
-
-    menuIcon.addEventListener('click', function () {
-        menuIcon.classList.toggle('bx-x');
-        navbar.classList.toggle('open');
-    });
-});
