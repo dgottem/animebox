280316482700f9cbc6ed04e1b49fb7c8d4f0d190
Gottem <60016015+dgottem@users.noreply.github.com>
2024-02-11 23:51:46 +0100

    minor additions

--- topheader.js
+++ topheader.js
@@ -6,4 +6,23 @@ document.addEventListener('DOMContentLoaded', function () {
         menuIcon.classList.toggle('bx-x');
         navbar.classList.toggle('open');
     });
+
+
+    const navAccIcon = document.getElementById('na-dropdown');
+    const navAccContent = document.getElementById('na-dropdown-content');
+
+    navAccIcon.addEventListener("click", function(){
+        if(navAccContent.style.display === "none" || navAccContent.style.display === ""){
+            navAccContent.style.display = "block";
+        }else{
+            navAccContent.style.display = "none";
+        }
+    });
+
+    this.body.addEventListener("click", function(Event){
+        if(Event.target !== navAccIcon && navAccContent && navAccContent.style.display === "block"){
+            navAccContent.style.display = "none";
+        }
+    })
+
 });
