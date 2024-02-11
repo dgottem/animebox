abe8dc20a80664d58955c2bcff24ba0974d3778a
Gottem <60016015+dgottem@users.noreply.github.com>
2024-02-11 17:00:54 +0100

    added general js

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    // Checks if the screen width is 2500px or wider
+    if (window.innerWidth >= 2500) {
+        alert("Due to the width of your screen, zoom in for a better experience.");
+    }
+};
