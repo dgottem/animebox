2d6eb61c15cb74cbabc829cab5bf92092c48e8d9
Gottem <60016015+dgottem@users.noreply.github.com>
2024-02-12 18:05:06 +0100

    minor changes

--- topheader.js
+++ topheader.js
@@ -11,9 +11,30 @@ document.addEventListener('DOMContentLoaded', function () {
     const navAccIcon = document.getElementById('na-dropdown');
     const navAccContent = document.getElementById('na-dropdown-content');
 
-    navAccIcon.addEventListener('click', function() {
+    navAccIcon.addEventListener('click', function () {
         navAccContent.classList.toggle('na-dc-open');
-    })
+    });
 
+    const searchIcon1 = document.getElementById('search-icon_1');
+    const searchContainer = document.querySelector('.search-box-container');
+
+    searchIcon1.addEventListener('click', function () {
+        if(searchContainer.style.display === "flex"){
+            searchContainer.style.display = "none";
+        }else{
+            searchContainer.style.display = "flex";
+        }
+    });
+
+    const bellIcon = document.getElementById('bell-icon');
+    const notiModal = document.getElementById('noti-modal');
+
+    bellIcon.addEventListener('click', function () {
+        if(notiModal.style.left !== 0 + "px"){
+            notiModal.style.left = 0;
+        }else{
+            notiModal.style.left = -100 + "%";
+        }
+    });
     
 });
