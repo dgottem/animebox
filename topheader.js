4d5744411e5aaaec81964b76ab54b7f1b25dbfbf
Gottem <60016015+dgottem@users.noreply.github.com>
2024-02-12 11:07:14 +0100

    minor changes

--- topheader.js
+++ topheader.js
@@ -4,25 +4,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
     menuIcon.addEventListener('click', function () {
         menuIcon.classList.toggle('bx-x');
-        navbar.classList.toggle('open');
+        navbar.classList.toggle('nav-open');
     });
 
 
     const navAccIcon = document.getElementById('na-dropdown');
     const navAccContent = document.getElementById('na-dropdown-content');
 
-    navAccIcon.addEventListener("click", function(){
-        if(navAccContent.style.display === "none" || navAccContent.style.display === ""){
-            navAccContent.style.display = "block";
-        }else{
-            navAccContent.style.display = "none";
-        }
-    });
-
-    this.body.addEventListener("click", function(Event){
-        if(Event.target !== navAccIcon && navAccContent && navAccContent.style.display === "block"){
-            navAccContent.style.display = "none";
-        }
+    navAccIcon.addEventListener('click', function() {
+        navAccContent.classList.toggle('na-dc-open');
     })
 
 });
