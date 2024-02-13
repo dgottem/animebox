235a504ccfa2b0b3dabe39523beb3c9648c05033
Gottem <60016015+dgottem@users.noreply.github.com>
2024-02-13 10:41:26 +0100

    minor changes

--- topheader.js
+++ topheader.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const screenWidth = window.innerWidth;
+
     const menuIcon = document.getElementById('menu-icon');
     const navbar = document.querySelector('.navbar');
 
@@ -8,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
 
-    const navAccIcon = document.getElementById('na-dropdown');
+    const navAccIcon = document.getElementById('na-dropdown-icon');
     const navAccContent = document.getElementById('na-dropdown-content');
 
     navAccIcon.addEventListener('click', function () {
@@ -18,23 +20,31 @@ document.addEventListener('DOMContentLoaded', function () {
     const searchIcon1 = document.getElementById('search-icon_1');
     const searchContainer = document.querySelector('.search-box-container');
 
+    const spotlightDiv = document.getElementById('spotlight');
+
     searchIcon1.addEventListener('click', function () {
-        if(searchContainer.style.display === "flex"){
-            searchContainer.style.display = "none";
-        }else{
+        const searchContainerCD = window.getComputedStyle(searchContainer).getPropertyValue('display');
+    
+        if(searchContainerCD === "none" && screenWidth < 1281){
             searchContainer.style.display = "flex";
+            spotlightDiv.style.marginTop = 45 + "px";
+        }else if(searchContainerCD === "none" && screenWidth >= 1281){
+            searchContainer.style.display = "flex";
+            spotlightDiv.style.marginTop = 40 + "px";
+        }else{
+            searchContainer.style.display = "none";
+            spotlightDiv.style.marginTop = 0 + "px";
         }
     });
+    
 
     const bellIcon = document.getElementById('bell-icon');
     const notiModal = document.getElementById('noti-modal');
 
-    bellIcon.addEventListener('click', function () {
-        if(notiModal.style.left !== 0 + "px"){
-            notiModal.style.left = 0;
-        }else{
-            notiModal.style.left = -100 + "%";
-        }
+    bellIcon.addEventListener("click", function () {
+        console.log("Bell icon clicked");
+        notiModal.classList.toggle('noti-modal-open');
     });
-    
+     
+
 });
