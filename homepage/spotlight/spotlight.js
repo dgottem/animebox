3f71c1adbfacf261f8ff931ccab02de00fad31c5
Gottem <60016015+dgottem@users.noreply.github.com>
2024-02-25 01:21:06 +0100

    modified image slider of the homepage and added a general maximum <body> width

--- homepage/spotlight/spotlight.js
+++ homepage/spotlight/spotlight.js
@@ -1,24 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-  let slider = document.querySelector('.slider .list');
-  let items = document.querySelectorAll('.slider .list .item');
-  let dots = document.querySelectorAll('.dots li');
+  let sliderList = document.getElementById('slider-list');
+  let sliderItems = document.querySelectorAll('.slider-item');
+  let sliderDots = document.querySelectorAll('#slider-dots li');
 
-  let lengthItems = items.length - 1;
-  let active = 0;
+  let lengthItems = sliderItems.length - 1;
+  let activeSlide = 0;
 
   let refreshInterval = setInterval(() => { nextSlide() }, 6000);
 
   function nextSlide() {
-    active = active + 1 <= lengthItems ? active + 1 : 0;
+    activeSlide = (activeSlide + 1 <= lengthItems) ? activeSlide + 1 : 0;
     reloadSlider();
   }
 
   function prevSlide() {
-    active = active - 1 >= 0 ? active - 1 : lengthItems;
+    activeSlide = (activeSlide - 1 >= 0) ? activeSlide - 1 : lengthItems;
     reloadSlider();
   }
 
-  $('.slider').swipe({
+  $('#slider').swipe({
     swipeLeft: function() {
       nextSlide();
     },
@@ -29,19 +29,19 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   function reloadSlider() {
-    slider.style.left = -items[active].offsetLeft + 'px';
+    sliderList.style.left = -sliderItems[activeSlide].offsetLeft + 'px';
 
-    let last_active_dot = document.querySelector('.dots li.active-dot');
-    last_active_dot.classList.remove('active-dot');
-    dots[active].classList.add('active-dot');
+    let lastActiveDot = document.querySelector('#slider-dots li.active-dot');
+    lastActiveDot.classList.remove('active-dot');
+    sliderDots[activeSlide].classList.add('active-dot');
 
     clearInterval(refreshInterval);
     refreshInterval = setInterval(() => { nextSlide() }, 6000);
   }
 
-  dots.forEach((li, key) => {
+  sliderDots.forEach((li, key) => {
     li.addEventListener('click', () => {
-      active = key;
+      activeSlide = key;
       reloadSlider();
     });
   });
