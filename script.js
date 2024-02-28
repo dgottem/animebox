21d9103168d87c5f9e4275e47d8e58a41f492d2b
Gottem <60016015+dgottem@users.noreply.github.com>
2024-02-28 19:01:20 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-window.onload = function () {
-    // Checks if the screen width is 2500px or wider
+window.onload = () => {
     if (window.innerWidth >= 2500) {
         alert("Due to the width of your screen, zoom in for a better experience.");
     }
---
+++ watch/zom-100-bucket-list-of-the-dead/zom-100-bucket-list-of-the-dead.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const seriesName = "zom_100_bucket_list_of_the_dead";
+    const savedSrc = localStorage.getItem(`${seriesName}_episodeSrc`);
+
+    if (savedSrc) {
+        document.getElementById('video-frame').src = savedSrc;
+    }
+
+    window.changeEpisode = (src) => {
+        const videoFrame = document.getElementById('video-frame');
+        videoFrame.src = src;
+
+        localStorage.setItem(`${seriesName}_episodeSrc`, src);
+    }
+
+    let currentEpisode = parseInt(localStorage.getItem(`${seriesName}_currentEpisode`)) || 1;
+    const totalEpisodes = 12;
+
+    const toPreviousVid = document.getElementById('to-previous-vid');
+    const toNextVid = document.getElementById('to-next-vid');
+
+    const currentWatch = document.getElementById('current-watch');
+    currentWatch.textContent = `Episode ${currentEpisode}`;
+
+    const episodeLinks = document.querySelectorAll('.ep-li-item a');
+    episodeLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            const episodeNumber = parseInt(link.getAttribute('data-epnum'));
+            currentWatch.textContent = `Episode ${episodeNumber}`;
+            localStorage.setItem(`${seriesName}_currentEpisode`, episodeNumber);
+        });
+    });
+
+    toPreviousVid.addEventListener('click', function() {
+        const currentEpisode = parseInt(currentWatch.textContent.split(' ')[1]);
+        if (currentEpisode > 1) {
+            const previousEpisode = currentEpisode - 1;
+            currentWatch.textContent = `Episode ${previousEpisode}`;
+            localStorage.setItem(`${seriesName}_currentEpisode`, previousEpisode);
+            document.querySelector(`.ep-li-item:nth-child(${previousEpisode}) a`).click();
+        }
+    });
+
+    toNextVid.addEventListener('click', function() {
+        const currentEpisode = parseInt(currentWatch.textContent.split(' ')[1]);
+        if (currentEpisode < totalEpisodes) {
+            const nextEpisode = currentEpisode + 1;
+            currentWatch.textContent = `Episode ${nextEpisode}`;
+            localStorage.setItem(`${seriesName}_currentEpisode`, nextEpisode);
+            document.querySelector(`.ep-li-item:nth-child(${nextEpisode}) a`).click();
+        }
+    });
+
+});
